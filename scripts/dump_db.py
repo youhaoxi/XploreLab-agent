a6e2e5af7b997496841390e8133be693293224f2
easonsshi <easonsshi@tencent.com>
2025-07-03 11:40:40 +0800

    up script

--- scripts/dump_db.py
+++ scripts/dump_db.py
@@ -1,8 +1,20 @@
 import asyncio
 import json
 import argparse
+
+import pandas as pd
+
 from utu.eval.data_manager.db_data_manager import DBDataManager
 from utu.config import ConfigLoader, EvalConfig
+
+
+async def get_stat(config: EvalConfig):
+    db = DBDataManager(config)
+    samples = await db.get_samples()
+    df = pd.DataFrame([sample.as_dict() for sample in samples])
+    # stat data group by stage
+    stat = df.groupby("stage").size()
+    print(stat)
 
 
 async def dump_exp_samples(config: EvalConfig, output_file: str, clear_records: bool = False):
@@ -17,9 +29,10 @@ async def dump_exp_samples(config: EvalConfig, output_file: str, clear_records: 
         await db.delete_samples(samples)
 
 
-if __name__ == "__main__":
+async def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--config_name", type=str, default="v01", help="Configuration name for evaluation.")
+    parser.add_argument("--op", type=str, choices=["stat", "dump"], default="dump", help="Option to run.")
     parser.add_argument("--exp_id", type=str, default=None, help="Experiment ID.")
     parser.add_argument("--output_file", type=str, default=None, help="Output file.")
     parser.add_argument("--clear_records", action="store_true", help="Clear records from db.")
@@ -31,5 +44,13 @@ if __name__ == "__main__":
 
     if not args.output_file:
         args.output_file = f"data/output_{config.exp_id}.jsonl"
+    
+    match args.op:
+        case "stat":
+            await get_stat(config)
+        case "dump":
+            await dump_exp_samples(config, output_file=args.output_file, clear_records=args.clear_records)
 
-    asyncio.run(dump_exp_samples(config, output_file=args.output_file, clear_records=args.clear_records))
+
+if __name__ == "__main__":
+    asyncio.run(main())
