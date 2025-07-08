662ca193b74cf16f2c15eb978290db577128bd4b
easonsshi <easonsshi@tencent.com>
2025-07-08 09:37:12 +0800

    add anthropic tool schema

--- utu/tools/bash_tool.py
+++ utu/tools/bash_tool.py
@@ -1,6 +1,17 @@
 """ 
 https://github.com/pexpect/pexpect
 @ii-agent/src/ii_agent/tools/bash_tool.py
+
+
+--- https://www.anthropic.com/engineering/swe-bench-sonnet ---
+Run commands in a bash shell\n
+* When invoking this tool, the contents of the \"command\" parameter does NOT need to be XML-escaped.\n
+* You don't have access to the internet via this tool.\n
+* You do have access to a mirror of common linux and python packages via apt and pip.\n
+* State is persistent across command calls and discussions with the user.\n
+* To inspect a particular line range of a file, e.g. lines 10-25, try 'sed -n 10,25p /path/to/the/file'.\n
+* Please avoid commands that may produce a very large amount of output.\n
+* Please run long lived commands in the background, e.g. 'sleep 10 &' or start a server in the background."
 """
 
 import re
