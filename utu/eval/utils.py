0a3b95cd6b260ba4e33afc04097014506eb0770b
easonsshi <easonsshi@tencent.com>
2025-07-08 19:58:11 +0800

    misc | parse_eval_config

---
+++ utu/eval/utils.py
@@ -0,0 +1,21 @@
+import argparse
+
+from utu.config import ConfigLoader, EvalConfig
+
+
+def parse_eval_config() -> EvalConfig:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config_name", type=str, default="v01", help="Configuration name for evaluation.")
+    parser.add_argument("--exp_id", type=str, default=None, help="Experiment ID.")
+    parser.add_argument("--agent_model", type=str, default=None, help="Agent model.")
+    parser.add_argument("--dataset", type=str, default=None, help="Dataset.")
+    args = parser.parse_args()
+    
+    config = ConfigLoader.load_eval_config(args.config_name)
+    if args.exp_id:
+        config.exp_id = args.exp_id
+    if args.agent_model:
+        config.agent.model.model = args.agent_model
+    if args.dataset:
+        config.dataset = args.dataset
+    return config
