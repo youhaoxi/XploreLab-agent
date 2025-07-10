82f707f91c529d228a15ffc4f31237c59e7d9363
easonsshi <easonsshi@tencent.com>
2025-07-10 12:04:37 +0800

    prune

--- utu/agents/__init__.py
+++ utu/agents/__init__.py
@@ -1,19 +1,5 @@
-# from .simple_env_agent import UTUSimpleEnvAgent
-# # from .tool_maker import UTUToolMakerAgent
-
-from ..config import AgentConfig, ConfigLoader
-
-
-# AGENT_MAP = {
-#     "simple": UTUSimpleAgent,
-#     "simple_env": UTUSimpleEnvAgent,
-# }
-# def build_agent(config: AgentConfig|str, *args, **kwargs) -> UTUSimpleAgent|UTUSimpleEnvAgent:
-#     # a simple wrapper to load agent config and build agent
-#     if isinstance(config, str):
-#         config = ConfigLoader.load_agent_config(config)
-#     assert config.type in AGENT_MAP, f"Unknown agent type: {config.type}"
-#     return AGENT_MAP[config.type](config, *args, **kwargs)
-
-
 from .simple_agent import SimpleAgent
+
+__all__ = [
+    "SimpleAgent",
+]
