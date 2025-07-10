76cb2b0314169ff8f678277f59fbfbc9cace46b2
easonsshi <easonsshi@tencent.com>
2025-07-10 14:50:04 +0800

    clean /eval

--- utu/eval/__init__.py
+++ utu/eval/__init__.py
@@ -1,6 +1,13 @@
 from .data import EvaluationSample, DBDataManager
 from .processer import BaseProcesser, PROCESSER_FACTORY
-from .data_processer import BaseProcesser, DATA_PROCESSER_FACTORY, MixedProcesser
 from .benchmarks.base_benchmark import BaseBenchmark
 # from .common import limit_concurrency, limit_concurrency_thread, process_with_threading, async_to_sync
-# from .runner import ExpRunner
+
+__all__ = [
+    "EvaluationSample",
+    "DBDataManager",
+    "BaseProcesser",
+    "PROCESSER_FACTORY",
+    "BaseBenchmark",
+]
+
