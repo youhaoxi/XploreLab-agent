343de9353d69a8ca733c30f50d360778c53b93fd
easonsshi <easonsshi@tencent.com>
2025-08-03 21:23:37 +0800

    #fix SimplifiedAsyncOpenAI

--- tests/utils/test_openai_utils.py
+++ tests/utils/test_openai_utils.py
@@ -2,7 +2,7 @@ from utu.utils import SimplifiedAsyncOpenAI, OpenAIUtils
 from utu.config import ConfigLoader
 
 
-config = ConfigLoader.load_model_config("v00")
+config = ConfigLoader.load_model_config("base")
 openai_client = SimplifiedAsyncOpenAI(**config.model_provider.model_dump())
 print(f"Testing {config.model_provider.model}, with api_key={config.model_provider.api_key[:5]}..., base_url={config.model_provider.base_url}")
 # messages = [{"role": "user", "content": "Tell a joke. And what is the weather like in Bogotá and Shanghai?"}]
@@ -37,7 +37,7 @@ async def test_model_stream():
         print(chunk.choices[0].delta)
 
 
-async def test_print_message():
+async def test_print():
     res = await openai_client.chat_completion(messages=messages, tools=tools, stream=False)
     OpenAIUtils.print_message(res.choices[0].message)
 
