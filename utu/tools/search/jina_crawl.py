67e68a712ed64b29a937be44f7ea0b2bb66bdf75
easonsshi <easonsshi@tencent.com>
2025-09-04 19:50:05 +0800

    trnsmr tool cache name

--- utu/tools/search/jina_crawl.py
+++ utu/tools/search/jina_crawl.py
@@ -15,14 +15,13 @@ class JinaCrawl:
 
     async def crawl(self, url: str) -> str:
         """standard crawl interface."""
-        return await self.get_content(url)
+        return await self.crawl_jina(url)
 
     @async_file_cache(expire_time=None)
-    async def get_content(self, url: str) -> str:
+    async def crawl_jina(self, url: str) -> str:
         # Get the content of the url
         params = {"url": url}
         async with aiohttp.ClientSession() as session:
             async with session.get(self.jina_url, headers=self.jina_header, params=params) as response:
                 text = await response.text()
-                # logger.info(f"[tool] get_content: {oneline_object(text)}...")
                 return text
