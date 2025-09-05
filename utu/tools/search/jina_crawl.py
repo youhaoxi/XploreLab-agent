f277b0d26a645bd140eeaf63f31dcf84a2aed9e6
easonsshi <easonsshi@tencent.com>
2025-09-05 11:39:32 +0800

    set JINA_API_KEY optional for jina crawl

--- utu/tools/search/jina_crawl.py
+++ utu/tools/search/jina_crawl.py
@@ -8,9 +8,13 @@ logger = get_logger(__name__)
 class JinaCrawl:
     def __init__(self, config: dict = None) -> None:
         self.jina_url = "https://r.jina.ai/"
-        self.jina_header = {
-            "Authorization": f"Bearer {EnvUtils.get_env('JINA_API_KEY')}",
-        }
+        self.jina_header = {}
+        api_key = EnvUtils.get_env("JINA_API_KEY", "")
+        if api_key:
+            self.jina_header["Authorization"] = f"Bearer {api_key}"
+        else:
+            # https://jina.ai/api-dashboard/rate-limit
+            logger.warning("Jina API key not found! Access rate may be limited.")
         config = config or {}
 
     async def crawl(self, url: str) -> str:
