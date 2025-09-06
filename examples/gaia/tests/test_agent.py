b35e1403bb1443f00a6bdc45bfce5f96ae818a2b
easonsshi <easonsshi@tencent.com>
2025-09-06 17:57:43 +0800

    add DocumentProcessingToolkit & reasoning_coding_agent

--- examples/gaia/tests/test_agent.py
+++ examples/gaia/tests/test_agent.py
@@ -9,8 +9,9 @@ agent = SimpleAgent(config=ConfigLoader.load_agent_config("simple_agents/gaia_re
 async def test_agent():
     await agent.build()
     print(f"agent with tools: {agent.tools}")
-    await agent.chat_streamed("What tools do you have?")
-    await agent.chat_streamed("analysis data/gaia/2023/validation/3da89939-209c-4086-8520-7eb734e6b4ef.xlsx")
+    # await agent.chat_streamed("What tools do you have?")
+    # await agent.chat_streamed("analysis data/gaia/2023/validation/3da89939-209c-4086-8520-7eb734e6b4ef.xlsx")
+    await agent.chat_streamed("analysis data/gaia/2023/validation/9b54f9d9-35ee-4a14-b62f-d130ea00317f.zip")
     await agent.cleanup()
 
 
