f4ddadb1234efe841f0d9cf7faf0e923708490e0
Mactarvish <451143003@qq.com>
2025-09-10 00:11:52 +0800

    [bugfix] fix cannot load gbk under pwsh

--- utu/utils/path.py
+++ utu/utils/path.py
@@ -78,5 +78,5 @@ class FileUtils:
                 fn += ".yaml"
             fn = DIR_ROOT / "utu" / "prompts" / fn
         assert fn.exists(), f"File {fn} does not exist!"
-        with fn.open() as f:
+        with open(fn, "r", encoding="utf-8") as f:
             return yaml.safe_load(f)
