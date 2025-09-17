742399887d4844384609642109f5cd03b649b954
easonsshi <easonsshi@tencent.com>
2025-09-17 18:35:04 +0800

    enhance FileEditToolkit

--- tests/tools/test_file_edit_toolkit.py
+++ tests/tools/test_file_edit_toolkit.py
@@ -1,6 +1,13 @@
 import pytest
 
+from utu.config import ConfigLoader
 from utu.tools import FileEditToolkit
+from utu.utils import DIR_ROOT
+
+CONTENT = """line 1
+line 2
+line 3
+"""
 
 DIFF = """<<<<<<< SEARCH
 line 1
@@ -11,15 +18,13 @@ line 1 edited!
 
 @pytest.fixture
 def file_edit_toolkit():
-    return FileEditToolkit(
-        config={
-            "work_dir": "/tmp/",
-            "backup_enabled": True,
-            "default_encoding": "utf-8",
-        }
-    )
+    config = ConfigLoader.load_toolkit_config("bash")
+    config.config["work_dir"] = str(DIR_ROOT / "data" / "test_bash")
+    return FileEditToolkit(config=config)
 
 
 async def test_edit_file(file_edit_toolkit: FileEditToolkit):
+    result = await file_edit_toolkit.write_file("test.txt", CONTENT)
+    print(result)
     result = await file_edit_toolkit.edit_file("test.txt", DIFF)
     print(result)
