ca95e914d29b9e39a8174823115eb1a9bc4221be
easonsshi <easonsshi@tencent.com>
2025-09-19 10:56:41 +0800

    update python tool schema: remove the `workdir` param

--- utu/tools/python_executor_toolkit.py
+++ utu/tools/python_executor_toolkit.py
@@ -1,9 +1,14 @@
+"""
+- [ ] polish _execute_python_code_sync
+"""
+
 import asyncio
 import base64
 import contextlib
 import glob
 import io
 import os
+import pathlib
 import re
 import traceback
 import uuid
@@ -139,23 +144,30 @@ class PythonExecutorToolkit(AsyncBaseToolkit):
     def __init__(self, config: ToolkitConfig | dict | None = None):
         super().__init__(config)
 
+        workspace_root = self.config.config.get("workspace_root", None)
+        if workspace_root is None:
+            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+            unique_id = str(uuid.uuid4())[:8]
+            workspace_root = f"/tmp/utu/python_executor/{timestamp}_{unique_id}"
+        self.setup_workspace(workspace_root)
+
+    def setup_workspace(self, workspace_root: str):
+        workspace_dir = pathlib.Path(workspace_root)
+        workspace_dir.mkdir(parents=True, exist_ok=True)
+        self.workspace_root = workspace_root
+
     @register_tool
-    async def execute_python_code(self, code: str, workdir: str = None, timeout: int = 30) -> dict:
+    async def execute_python_code(self, code: str, timeout: int = 30) -> dict:
         """
         Executes Python code and returns the output.
 
         Args:
             code (str): The Python code to execute.
-            workdir (str): The working directory for the execution. Defaults to "./run_workdir".
             timeout (int): The execution timeout in seconds. Defaults to 30.
 
         Returns:
             dict: A dictionary containing the execution results.
         """
-        if workdir is None:
-            timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-            unique_id = str(uuid.uuid4())[:8]
-            workdir = f"/tmp/utu/python_executor/{timestamp}_{unique_id}"
         loop = asyncio.get_running_loop()
         try:
             return await asyncio.wait_for(
@@ -163,7 +175,7 @@ class PythonExecutorToolkit(AsyncBaseToolkit):
                     None,  # Use the default thread pool executor
                     _execute_python_code_sync,
                     code,
-                    workdir,
+                    str(self.workspace_root),
                 ),
                 timeout=timeout,
             )
