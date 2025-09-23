e44bb4994aa16e255487909db4219bb03323144f
easonsshi <easonsshi@tencent.com>
2025-09-23 16:00:43 +0800

    support mcp tools in agent_generator

--- tests/tools/test_mcp.py
+++ tests/tools/test_mcp.py
@@ -1,38 +1,12 @@
-from agents.mcp import MCPServerSse, MCPServerStdio, MCPServerStreamableHttp
-
-from utu.config import ConfigLoader, ToolkitConfig
-
-
-def get_mcp_server(config: ToolkitConfig):
-    assert config.mode == "mcp", f"config mode must be 'mcp', got {config.mode}"
-    match config.mcp_transport:
-        case "stdio":
-            return MCPServerStdio(
-                params=config.config,
-                name=config.name,
-                client_session_timeout_seconds=config.mcp_client_session_timeout_seconds,
-            )
-        case "sse":
-            return MCPServerSse(
-                params=config.config,
-                name=config.name,
-                client_session_timeout_seconds=config.mcp_client_session_timeout_seconds,
-            )
-        case "streamable_http":
-            return MCPServerStreamableHttp(
-                params=config.config,
-                name=config.name,
-                client_session_timeout_seconds=config.mcp_client_session_timeout_seconds,
-            )
-        case _:
-            raise ValueError(f"Unsupported mcp transport: {config.mcp_transport}")
+from utu.config import ConfigLoader
+from utu.tools.utils import get_mcp_server, get_mcp_tools_schema
 
 
 async def test_mcp():
     # config_name = "mcp/time"
     # action = [("get_current_time", {"timezone": "Asia/Shanghai"})]
     # config_name = "mcp/sequentialthinking"
-    # action = [("sequentialthinking", {"thought": "step 1", "nextThoughtNeeded": False, "thoughtNumber": 1, "totalThoughts": 1})]
+    # action = [("sequentialthinking", {"thought": "step 1", "nextThoughtNeeded": False, "thoughtNumber": 1, "totalThoughts": 1})]  # ruff: noqa: E501
 
     # config_name = "mcp/context7"
     # action = [("get-library-docs", {"context7CompatibleLibraryID": "/mongodb/docs"})]
@@ -51,3 +25,11 @@ async def test_mcp():
             print(f"Calling tool: {name} with params: {params}")
             res = await mcp_server.call_tool(name, params)
             print(f"> res: {res}")
+
+
+async def test_get_mcp_tools_schema():
+    config_name = "mcp/github"
+    config = ConfigLoader.load_toolkit_config(config_name)
+    tools_map = await get_mcp_tools_schema(config)
+    for name, schema in tools_map.items():
+        print(f"Tool: {name}, schema: {schema}")
